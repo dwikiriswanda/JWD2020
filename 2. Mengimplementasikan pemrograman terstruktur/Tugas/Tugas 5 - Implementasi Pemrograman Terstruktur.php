10386f8270de8a0a8bb4ee20b3d97516d28e2c08
dwikiriswanda <m.dwikiriswanda@gmail.com>
2020-06-09 13:59:48 +0700

    Add comment

--- 2. Mengimplementasikan pemrograman terstruktur/Tugas/Tugas 5 - Implementasi Pemrograman Terstruktur.php
+++ 2. Mengimplementasikan pemrograman terstruktur/Tugas/Tugas 5 - Implementasi Pemrograman Terstruktur.php
@@ -4,22 +4,22 @@ $bilangan1 = 9;
 $bilangan2 = 3;
 
 function penjumlahan($bilangan1, $bilangan2) {
-    $jumlah = $bilangan1 + $bilangan2;
+    $jumlah = $bilangan1 + $bilangan2; // menambahkan bilangan ke-1 dengan bilangan ke-2
     echo "hasil penjumlahan adalah : " . $jumlah;
 }
 
 function pengurangan($bilangan1, $bilangan2) {
-    $kurang = $bilangan1 - $bilangan2;
+    $kurang = $bilangan1 - $bilangan2; // mengurangi bilangan ke-1 dengan bilangan ke-2
     echo "hasil pengurangan adalah : " . $kurang;
 }
 
 function perkalian($bilangan1, $bilangan2) {
-    $kali = $bilangan1 * $bilangan2;
+    $kali = $bilangan1 * $bilangan2; // mengalikan bilangan ke-1 dengan bilangan ke-2
     echo "hasil perkalian adalah : " . $kali;
 }
 
 function pembagian($bilangan1, $bilangan2) {
-    $bagi = $bilangan1 / $bilangan2;
+    $bagi = $bilangan1 / $bilangan2; // membagi bilangan ke-1 dengan bilangan ke-2
     echo "hasil pembagian adalah : " . $bagi;
 }
 
@@ -27,12 +27,12 @@ echo "bilangan 1 : " . $bilangan1;
 echo "<br>";
 echo "bilangan 2 : " . $bilangan2;
 echo "<hr> <hr>";
-penjumlahan($bilangan1, $bilangan2);
+penjumlahan($bilangan1, $bilangan2); // memanggil fungsi penjumlahan dengan parameter bilangan1 dan bilangan2
 echo "<br>";
-pengurangan($bilangan1, $bilangan2);
+pengurangan($bilangan1, $bilangan2); // memanggil fungsi pengurangan dengan parameter bilangan1 dan bilangan2
 echo "<br>";
-perkalian($bilangan1, $bilangan2);
+perkalian($bilangan1, $bilangan2); // memanggil fungsi perkalian dengan parameter bilangan1 dan bilangan2
 echo "<br>";
-pembagian($bilangan1, $bilangan2);
+pembagian($bilangan1, $bilangan2); // memanggil fungsi pembagian dengan parameter bilangan1 dan bilangan2
 
 ?>
